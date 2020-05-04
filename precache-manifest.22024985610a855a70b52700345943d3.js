952118bfde90cb5360cf0aa799ee2eafa5046196
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-05-04 14:23:06 +0200

    Updates

---
+++ precache-manifest.22024985610a855a70b52700345943d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ffa62a20dfa1579864ff8732f1d144a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc2c7e02c0c5f089024d",
+    "url": "/static/css/main.4d07127a.chunk.css"
+  },
+  {
+    "revision": "310705432fce5c03c790",
+    "url": "/static/js/2.7d46b806.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d46b806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2c7e02c0c5f089024d",
+    "url": "/static/js/main.6d8c1592.chunk.js"
+  },
+  {
+    "revision": "51325dbb1702b163e5f3",
+    "url": "/static/js/runtime-main.a41c6e3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
