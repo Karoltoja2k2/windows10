53b95af0775388205c203388cd49e1cff96a2cd7
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-05-06 00:22:06 +0200

    file properties as prop

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FileExplorer from './FileExplorer';
 import '../scss/icon.scss'
 import test from '../media/logo192.png';
 
 
-const Icon = () => {
-    return(
-        <div className="icon">
+const Icon = (props: any) => {
+    function OpenFileEvent(event: any) {
+        props.OpenWindow(props.file)
+    }
+
+    return (
+        <div className="icon" onDoubleClick={(e) => OpenFileEvent(e)}>
             <div className="bg"></div>
-            <img src={ test } alt="fileIcon" />
-            <label>Mineswepper</label>
+            <img src={test} alt="fileIcon" />
+            <label>{props.file.title}{props.file.extension}</label>
         </div>
     );
 }
