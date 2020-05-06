cea01254776b89423a25ce1faeffef954e111738
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-05-06 13:18:33 +0200

    display icons in folders

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from 'react';
 import '../scss/fileExplorer.scss'
 import FileStructure from '../media/fileStructure.json'
 
+import Icon from './Icon'
+
 
 const FileExplorer = (props:any) => {
-    console.log(props.path)
-    var iconsInFolder = FileStructure.filter(x => x.path == props.file.path + props.file.title + '/')
-    console.log(iconsInFolder)
+    var iconsInFolder = FileStructure.filter(x => x.path == props.fileObj.file.contentPath)
     return (
         <div className="explorerContainer">
             <div className="toolBar">
 
             </div>
-            <div className="filesContainer">
-
+            <div className="iconGrid">
+                {
+                    iconsInFolder &&
+                    iconsInFolder.map((file:any, index:number) => (
+                        <Icon fileObj={{file: file, id:props.fileObj.id}} Navigate={props.Navigate}/>
+                    ))
+                }
             </div>
         </div>
     );
