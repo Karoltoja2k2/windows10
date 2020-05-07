c3116090c70e2268fd9be3acf497b150b40766f2
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-05-07 15:34:04 +0200

    minor changes in styles, taskbaritem component

---
+++ src/components/TaskBarItem.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import '../scss/desktop.scss'
+
+const TaskBarItem = () => {
+
+    return(
+        <div className="taskBarItem">
+
+        </div>
+    )
+}
+
+export default TaskBarItem
