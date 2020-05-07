c1763f6033dfcca805602d9fb78ad437481e67c8
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-05-07 16:48:41 +0200

    taskbar style

--- src/components/TaskBarItem.tsx
+++ src/components/TaskBarItem.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import '../scss/desktop.scss'
+import Logo from '../media/logo192.png'
 
 const TaskBarItem = () => {
 
     return(
         <div className="taskBarItem">
-
+            <img src={Logo} />
+            <label>Test</label>
         </div>
     )
 }
