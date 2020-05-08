7393232575a1110ab3121aa35b74e03fc8dc6eb6
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-05-08 16:47:22 +0200

    props structure simplified

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -6,13 +6,13 @@ import test from '../media/logo192.png';
 
 const Icon = (props: any) => {
     function OpenFileEvent(event: any) {
-        props.Navigate(props.fileReference)
+        props.Navigate(props.id, props.file)
     }
     return (
         <div className="icon" onDoubleClick={(e) => OpenFileEvent(e)}>
             <div className="bg"></div>
             <img src={test} alt="fileIcon" />
-            <label>{props.fileReference.file.title}{props.fileReference.file.extension}</label>
+            <label>{props.file.title}{props.file.extension}</label>
         </div>
     );
 }
--- src/components/PhotoDisplay.tsx
+++ src/components/PhotoDisplay.tsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react'
+import WindowBase from './WindowBase'
 
 
-const PhotoDisplay = (props:any) => {
-    return(
-        <div></div>
+const PhotoDisplay = (props: any) => {
+    return (
+        <WindowBase
+            id={props.id}
+            title={props.file.title}
+            style={props.style}
+            WindowManagement={props.WindowManagement}
+        >
+            <div></div>
+        </WindowBase>
     );
 }
 
