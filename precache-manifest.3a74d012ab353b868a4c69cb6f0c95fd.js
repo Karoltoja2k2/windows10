0a45c171eb9cdf040cbadc0ebe288ac44df81b7f
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-05-29 18:57:32 +0200

    Updates

--- precache-manifest.3a74d012ab353b868a4c69cb6f0c95fd.js
+++ precache-manifest.3a74d012ab353b868a4c69cb6f0c95fd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e228b77d76fefd7a38ab388c40fcff5",
+    "revision": "c75492d852544dec7857847995998fb3",
     "url": "/windows95/index.html"
   },
   {
-    "revision": "6db374d46008db1f5818",
-    "url": "/windows95/static/css/main.5af383f2.chunk.css"
+    "revision": "fa48ab71151b42cc33e6",
+    "url": "/windows95/static/css/main.10e8d916.chunk.css"
   },
   {
-    "revision": "96361bad8758bb5e70fb",
-    "url": "/windows95/static/js/2.8c8b9e06.chunk.js"
+    "revision": "beca87289bceca578ad6",
+    "url": "/windows95/static/js/2.68a13b8b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/windows95/static/js/2.8c8b9e06.chunk.js.LICENSE.txt"
+    "url": "/windows95/static/js/2.68a13b8b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6db374d46008db1f5818",
-    "url": "/windows95/static/js/main.af254771.chunk.js"
+    "revision": "fa48ab71151b42cc33e6",
+    "url": "/windows95/static/js/main.6a63f39a.chunk.js"
   },
   {
     "revision": "9ba2c22b2460af08afd9",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "7471f30760e74c9659984627f846cabe",
     "url": "/windows95/static/media/bg.7471f307.jpg"
+  },
+  {
+    "revision": "ca17c1d0451117324666a90f7537cc91",
+    "url": "/windows95/static/media/calibril.ca17c1d0.ttf"
   },
   {
     "revision": "620daf39069c2bccc7d64b2d1cd8c780",
