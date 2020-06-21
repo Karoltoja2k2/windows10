8fc88e658574bfc5aa89bad278fe7b59c8e42f23
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-06-21 12:52:30 +0200

    desktop icon uses it's own icon from file structure

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import '../scss/icon.scss'
-import test from '../media/logo192.png';
 
 
 const Icon = (props: any) => {
@@ -10,7 +9,7 @@ const Icon = (props: any) => {
     return (
         <div className="icon" onDoubleClick={(e) => OpenFileEvent(e)}>
             <div className="bg"></div>
-            <img src={test} alt="fileIcon" />
+            <img src={props.file.iconsrc} alt="fileIcon" />
             <label>{props.file.title}{props.file.extension}</label>
         </div>
     );
