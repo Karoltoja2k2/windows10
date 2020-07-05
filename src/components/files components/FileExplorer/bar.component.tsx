ba4d1f7bb7bc815b53e2f16b99f37fc3f0101c9a
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-07-05 18:20:59 +0200

    added search result dropdown with icons, open/close , search, styles;;;

--- src/components/files components/FileExplorer/bar.component.tsx
+++ src/components/files components/FileExplorer/bar.component.tsx
@@ -11,10 +11,12 @@ import {
 	faAngleDown,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FileIcon from "../../fileIcon/FileIcon.component";
 
 const Bar = (props: any) => {
 	const [search, setSearch] = useState({
 		path: props.file.path + props.file.title,
+		showResults: false,
 		results: Array<File>(),
 	});
 
@@ -26,26 +28,40 @@ const Bar = (props: any) => {
 	}, [props.file]);
 
 	const searchFile = (path: string) => {
+		if (!path) {
+			setSearch({
+				...search,
+				showResults: false,
+				results: [],
+			});
+			return;
+		}
 		console.log(path);
 		let files = Files.filter((x) =>
 			x.title.toLowerCase().includes(path.toLowerCase())
 		);
-		if (files.length > 0) {
-			setSearch({
-				...search,
-				results: files,
-			});
-		}
-		console.log(files);
+		setSearch({
+			...search,
+			showResults: true,
+			results: files,
+		});
 	};
+
+	function Navigate(id:number, file:File) {
+		setSearch({
+			...search,
+			showResults: false,
+		});
+		props.Navigate(id, file)
+	}
 
 	return (
 		<div className="container__bar">
 			<button
 				className={
 					props.file.prevFolder != null
-						? "button--enabled"
-						: "button--disabled"
+						? "bar__button bar__button--enabled"
+						: "bar__button bar__button--disabled"
 				}
 				onClick={() => {
 					props.previousFolder();
@@ -53,40 +69,58 @@ const Bar = (props: any) => {
 			>
 				<FontAwesomeIcon icon={faArrowLeft} />
 			</button>
-			<button className="button--disabled">
+			<button className="bar__button bar__button--disabled">
 				<FontAwesomeIcon icon={faArrowRight} />
 			</button>
-			<button className="button--disabled">
+			<button className="bar__button bar__button--disabled">
 				<FontAwesomeIcon icon={faAngleDown} />
 			</button>
-			<button className="button--disabled">
+			<button className="bar__button bar__button--disabled">
 				<FontAwesomeIcon icon={faArrowUp} />
 			</button>
 
 			<div className="bar__search--drive">
-				<input
-					type="text"
-					onChange={(e) => {
-						searchFile(e.target.value);
-					}}
-					onKeyDownCapture={(e) => {
-						if (e.key === "Enter") {
-							searchFile(search.path);
-						}
-					}}
-					placeholder={search.path}
-				/>
+				<div className="search__input">
+					<input
+						type="text"
+						onChange={(e) => {
+							console.log(e.target.value);
+							searchFile(e.target.value);
+						}}
+						onKeyDownCapture={(e) => {
+							if (e.key === "Enter") {
+								searchFile(search.path);
+							}
+						}}
+						placeholder={search.path}
+					/>
+					<button
+						className="bar__button bar__button--enabled"
+						onClick={() => {
+							setSearch({
+								...search,
+								showResults: !search.showResults
+							})
+						}}
+					>
+						<FontAwesomeIcon icon={faAngleDown} />
+					</button>
+				</div>
 				<div
 					className={
-						search.results.length > 0
+						search.showResults
 							? "search__results--active search__results"
 							: "search__results"
 					}
 				>
-					{search.results.map((result) => (
-						<button className="results__item">
-							<label className="">{result.title}</label>
-						</button>
+					{search.results.map((result: File, index: number) => (
+						<FileIcon
+							type="searchResult"
+							file={result}
+							id={props.id}
+							key={index}
+							Navigate={Navigate}
+						/>
 					))}
 				</div>
 			</div>
