47f714909973f28a645f673fa2916653240a0e0c
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-07-05 18:22:10 +0200

    add scrollbars, featured files in fileexplorer

--- src/components/files components/FileExplorer/content.component.tsx
+++ src/components/files components/FileExplorer/content.component.tsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
 import FileIcon from "../../fileIcon/FileIcon.component";
+import Files from '../../../models/fileStructure2'
 import "./content.scss";
 
 const Content = (props: any) => {
+
+	let files = Files;
+
 	return (
 		<div className="container__content">
-			<div className="content--left"></div>
+			<div className="content--left scrollbar--dark">
+				{files.map((file: any, index: number) => (
+						<FileIcon
+							type="inrow"
+							file={file}
+							id={props.id}
+							key={index}
+							Navigate={props.Navigate}
+						/>
+					))}
 
-			<div className="content--right">
+			</div>
+
+			<div className="content--right scrollbar--dark">
 				{props.iconsInFolder &&
 					props.iconsInFolder.map((file: any, index: number) => (
 						<FileIcon
