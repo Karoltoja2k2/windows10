12dcc838c0a71fa34c78c1798d4ed6f3e91a0f2f
karol.zedzian <karol.zedzian@rad4m.com>
2020-07-24 20:33:58 +0200

    before task manager

--- src/components/files components/snake/game.component.tsx
+++ src/components/files components/snake/game.component.tsx
@@ -2,149 +2,209 @@ import React, { useState, useEffect } from "react";
 import "./snake.scss";
 
 import Point from "./game objects/Point";
-import Head from "./game objects/Head";
-import BodyPart from "./game objects/BodyPart";
+// import Head from "./game objects/Head";
+// import BodyPart from "./game objects/BodyPart";
 // import Fruit from "./game objects/Fruit";
-import SnakeObject from "./game objects/SnakeObject";
+// import SnakeObject from "./game objects/SnakeObject";
 
 import Field from "./field.component";
+import { create } from "domain";
+// import Head from "./game objects/Head";
 
-interface Fruit{
-	cords:Point
+interface FieldBase {
+    cords: Point;
 }
 
+interface Fruit extends FieldBase {}
+
+interface BodyPart extends FieldBase {
+    nextBody?: BodyPart;
+}
+
+interface Head extends BodyPart {
+    direction: Point;
+    angle: number;
+}
+
+interface Snake {
+    head: Head;
+    body: BodyPart[];
+    tail: BodyPart;
+}
 
 const Game = (props: any) => {
-	const [size, setSize] = useState({
-		X: 25,
-		Y: 25,
-	});
+    const [size, setSize] = useState({
+        X: 25,
+        Y: 25,
+    });
 
-	const [fruitArray, setFruitArray] = useState([
-		{
-			cords: new Point(5,5)
-		},
-		{
-			cords: new Point(15,15)
-		}
-	]);
+    const [run, setRun] = useState(false);
+    const [fruits, setFruits] = useState<Fruit[]>([]);
+    const [snake, setSnake] = useState<Snake>();
+    const [clock, setClock] = useState(0);
 
-	useEffect(() => {
-		console.log(fruitArray)
-	}, [fruitArray])
-	const [snakeBody, setSnakeBody] = useState(SnakeObject);
+    useEffect(() => {
+		tick();			
+	}, [run]);
 
-	function spawnFruit() {
-
-		let x = Math.round(Math.random() * size.X);
-		let y = Math.round(Math.random() * size.Y);
-
-		let newArray = fruitArray.concat({
-			cords: new Point(x, y)
-		})
-
-		setFruitArray(newArray);
-	}
-
-	function renderGrid() {
-		return snakeBody.body.map((body) => {
-			return <Field field={body} style={"body"} />;
-		});
-	}
-	useEffect(() => {
-		spawnFruit();
-
-		setInterval(() => {
-			moveSnake();
-		}, 100);
+    useEffect(() => {
+		NewGame()
 	}, []);
-
-	function moveSnake() {
-		let snake = snakeBody;
-
-		let nextCord = snake.head.cords.AddPoint(snake.head.direction);
-		let eat = false;
-		console.log(fruitArray.length)
-		fruitArray.forEach((fruit) => {
-			if (fruit.cords.EqualTo(nextCord)) {
-				setFruitArray(fruitArray.filter((x) => x !== fruit));
-				eat = true;
-				return;
-			}
-		});
-
-		if (eat) {
-			let oldTailPnt = new Point(snake.tail.cords.X, snake.tail.cords.Y);
-			let oldTailRef = snake.tail;
-			moveBody(snake.tail);
-
-			snake.body.push(snake.tail);
-			snake.tail = new BodyPart(oldTailPnt, oldTailRef);
-			snake.head.Move();
-		} else {
-			moveBody(snake.tail);
-			snake.head.Move();
-		}
-
-		setSnakeBody({ head: snake.head, body: snake.body, tail: snake.tail });
+	
+	function NewGame(){
+		spawnFruit();
+        createSnake();
+		setRun(true);
 	}
 
-	function moveBody(bodyPart: BodyPart) {
-		if (bodyPart.nextBody) {
-			bodyPart.cords = bodyPart.nextBody.cords;
+    function tick() {
+        if (run) {
+            moveSnake();
+            setTimeout(() => {
+                tick();
+            }, 100);
+        }
+    }
 
-			moveBody(bodyPart.nextBody);
-		}
-	}
+    function createSnake() {
+        let head: Head = {
+            cords: new Point(10, 10),
+            direction: new Point(1, 0),
+            angle: 0,
+        };
+        let bodyPart: BodyPart = {
+            cords: new Point(9, 10),
+            nextBody: head,
+        };
+        let tail: BodyPart = {
+            cords: new Point(8, 10),
+            nextBody: bodyPart,
+        };
 
-	// function turnLeft(){
-	//     let snake = snakeBody;
-	//     snake.head.TurnLeft()
+        setSnake({
+            head: head,
+            body: [bodyPart],
+            tail: tail,
+        });
+    }
 
-	// 	setSnakeBody({ head: snake.head, body: snake.body});
-	// }
+    function spawnFruit() {
+        let x = Math.round(Math.random() * size.X);
+        let y = Math.round(Math.random() * size.Y);
 
-	// function turnRight(){
-	//     let snake = snakeBody;
-	//     snake.head.TurnRight()
+        let newArray = fruits.concat({
+            cords: new Point(x, y),
+        });
 
-	// 	setSnakeBody({ head: snake.head, body: snake.body});
-	// }
+        setFruits(newArray);
+    }
 
-	return (
-		<div
-			className="snake__container"
-			tabIndex={1}
-			onLoad={(e) => e.currentTarget.focus()}
-			onKeyDown={(e) => {
-				if (e.key === "a") {
-					snakeBody.head.TurnLeft();
-				} else if (e.key === "d") {
-					snakeBody.head.TurnRight();
-				}
-			}}
-		>
-			<div className="grid">
-				<Field field={snakeBody.head} style={"head"} />
-				{renderGrid()}
-				<Field field={snakeBody.tail} style={"tail"} />
+    function moveSnake() {
+        let snakeCopy: Snake | undefined = snake;
 
-				{fruitArray.map((fruit) => (
-					<Field field={fruit} style={"fruit"} />
-				))}
+        if (snakeCopy && run) {
+            let nextCord = snakeCopy.head.cords.AddPoint(
+                snakeCopy.head.direction
+            );
+            let eat = false;
+            fruits.forEach((fruit) => {
+                if (fruit.cords.EqualTo(nextCord)) {
+                    setFruits(fruits.filter((x) => x !== fruit));
+                    eat = true;
+                    return;
+                }
+            });
 
-				<button
-					className=""
-					onClick={() => {
-						setSnakeBody(SnakeObject);
-					}}
-				>
-					Restart
-				</button>
-				<label>{snakeBody.body.length}</label>
-			</div>
-		</div>
-	);
+            if (eat) {
+                spawnFruit();
+
+                let oldTailPnt = new Point(
+                    snakeCopy!.tail.cords.X,
+                    snakeCopy!.tail.cords.Y
+                );
+                let oldTailRef = snakeCopy!.tail;
+                moveBody(snakeCopy!.tail);
+
+                snakeCopy!.body.push(snakeCopy!.tail);
+                snakeCopy!.tail = {
+                    cords: oldTailPnt,
+                    nextBody: oldTailRef,
+                };
+                snakeCopy.head = moveHead(snakeCopy!.head);
+            } else {
+                moveBody(snakeCopy!.tail);
+                snakeCopy.head = moveHead(snakeCopy!.head);
+            }
+
+            setSnake({
+                head: snakeCopy!.head,
+                body: snakeCopy!.body,
+                tail: snakeCopy!.tail,
+            });
+        }
+    }
+
+    function moveHead(head: Head) {
+        head.cords = head.cords.AddPoint(head.direction);
+        return head;
+    }
+
+    function moveBody(bodyPart: BodyPart) {
+        if (bodyPart.nextBody) {
+            bodyPart.cords = bodyPart.nextBody.cords;
+
+            moveBody(bodyPart.nextBody);
+        }
+    }
+
+    function Turn(head: Head, angle: number) {
+        head.angle += angle;
+        let angleRad = head.angle * (Math.PI / 180);
+
+        head.direction = new Point(
+            1 * Math.round(Math.cos(angleRad)),
+            -1 * Math.round(Math.sin(angleRad))
+        );
+    }
+
+    return (
+        <div
+            className="snake__container"
+            tabIndex={1}
+            onLoad={(e) => e.currentTarget.focus()}
+            onKeyDown={(e) => {
+                if (e.key === "a") {
+                    Turn(snake!.head, 90);
+                } else if (e.key === "d") {
+                    Turn(snake!.head, -90);
+                }
+            }}
+        >
+            {snake !== null && run && (
+                <div className="grid">
+                    <Field field={snake!.head} style={"head"} />
+                    {snake!.body.map((body: BodyPart, index: number) => (
+                        <Field field={body} style={"body"} key={index} />
+                    ))}
+                    <Field field={snake!.tail} style={"tail"} />
+
+                    {fruits.map((fruit: Fruit, index: number) => (
+                        <Field field={fruit} style={"fruit"} key={index} />
+                    ))}
+
+                    <button
+                        className=""
+                        onClick={() => {
+                            NewGame()
+                        }}
+                    >
+                        Restart
+                    </button>
+                    <label>{snake!.body.length + 2}</label>
+                </div>
+            )}
+        </div>
+    );
 };
 
-export default React.memo(Game);
+export default Game;
--- src/components/files components/snake/snake.component.tsx
+++ src/components/files components/snake/snake.component.tsx
@@ -2,23 +2,20 @@ import React, { useState, useEffect } from "react";
 import "./snake.scss";
 
 import WindowBase from "../WindowBase";
-import Game from "./game.component"
+import Game from "./game.component";
 // import Field from "./field.component"
 
-
-
 const Snake = (props: any) => {
-
-	return (
-		<WindowBase
-			id={props.id}
-			file={props.file}
-			windowProps={props.windowProps}
-			WindowManagement={props.WindowManagement}
-		>
-			<Game />
-		</WindowBase>
-	);
+    return (
+        <WindowBase
+            id={props.id}
+            file={props.file}
+            windowProps={props.windowProps}
+            WindowManagement={props.WindowManagement}
+        >
+            <Game />
+        </WindowBase>
+    );
 };
 
-export default Snake;
+export default React.memo(Snake);
